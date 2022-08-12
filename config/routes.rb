d148dabd7b6df240f328c5ce56df42eafe95f239
yymmyymm <109639114+yymmyymm@users.noreply.github.com>
2022-08-12 11:47:27 +0000

    mada

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  root to: 'homes#top'
+  devise_for :users
+  get 'homes/about' => 'homes#about' ,as: 'about'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
